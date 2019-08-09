10ea417b3586cdae0be316789cd65c6165a5ada1
wziww <maoshuai@maoshuaideMacBook-Pro.local>
2019-08-09 17:09:01 +0800

    test

--- cpp/net.cpp
+++ cpp/net.cpp
@@ -87,19 +87,12 @@ void *handleAccept(void *ptr)
         sprintf(len_buffer, "%d", len);
         _write(fd, len_buffer, sizeof(len_buffer));
         _write(fd, CRLF, sizeof(CRLF));
-        for (map<string, int>::reverse_iterator iter = cmdCount.rbegin(); iter != cmdCount.rend(); iter++)
+        for (auto iter : cmdCount)
         {
-          char *buffer = new char[sizeof(iter->first)];
-          std::cout << iter->first << std::endl;
-          char v_buffer[sizeof(iter->second)];
+          char v_buffer[sizeof(iter.second)];
           memset(v_buffer, 0, sizeof(v_buffer));
-          sprintf(v_buffer, "%d", iter->second);
-          for (int i = 0; i < sizeof(iter->first); i++)
-          {
-            std::cout << iter->first[i] << std::endl;
-            buffer[i] = iter->first[i];
-          }
-          _write(fd, buffer, sizeof(buffer));
+          sprintf(v_buffer, "%d", iter.second);
+          _write(fd, iter.first.c_str(), sizeof(iter.first));
           _write(fd, CRLF, sizeof(CRLF));
           _write(fd, v_buffer, sizeof(v_buffer));
           _write(fd, CRLF, sizeof(CRLF));
