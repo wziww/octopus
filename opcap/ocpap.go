a1fb657cd3301373f73ef720fc25ccc05a029805
wziww <maoshuai@maoshuaideMacBook-Pro.local>
2019-08-19 19:23:46 +0800

    Refactoring tcp

--- opcap/ocpap.go
+++ opcap/ocpap.go
@@ -1,6 +1,7 @@
 package opcap
 
 import (
+	"bufio"
 	"net"
 	"octopus/log"
 	"strconv"
@@ -11,20 +12,36 @@ var client map[string]*OConn
 
 // OConn ...
 type OConn struct {
-	mutex sync.Mutex
-	conn  net.Conn
+	mutex   sync.Mutex
+	address string
+	fd      net.Conn
+	rd      *reader
+	wr      *writer
+}
+
+type writer struct {
+	wr *bufio.Writer
+}
+type reader struct {
+	rd   *bufio.Reader
+	_buf []byte
 }
 
 var (
 	mutex sync.Mutex
 	// CRLF ...
 	CRLF = []byte("\r\n")
-	// MAXSIZE tcp max size
-	MAXSIZE = 65535
 )
 
 func init() {
 	client = make(map[string]*OConn)
+}
+
+func connGet(conn *OConn) {
+	conn.mutex.Lock()
+}
+func connRelease(conn *OConn) {
+	conn.mutex.Unlock()
 }
 
 // CreateOrGetClient ...
@@ -32,10 +49,18 @@ func CreateOrGetClient(address string) (c *OConn, err error) {
 	mutex.Lock()
 	defer mutex.Unlock()
 	if client[address] == nil {
-		conn, e := net.Dial("tcp", address)
+		fd, e := net.Dial("tcp", address)
 		if e == nil {
 			client[address] = &OConn{
-				conn: conn,
+				address: address,
+				fd:      fd,
+				wr: &writer{
+					wr: bufio.NewWriter(fd),
+				},
+				rd: &reader{
+					rd:   bufio.NewReader(fd),
+					_buf: make([]byte, 64),
+				},
 			}
 		}
 		return nil, e
@@ -43,52 +68,50 @@ func CreateOrGetClient(address string) (c *OConn, err error) {
 	return client[address], nil
 }
 
+// RefreshClient ...
+func RefreshClient(address string) (c *OConn, err error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	delete(client, address)
+	fd, e := net.Dial("tcp", address)
+	if e == nil {
+		client[address] = &OConn{
+			address: address,
+			fd:      fd,
+			wr: &writer{
+				wr: bufio.NewWriter(fd),
+			},
+			rd: &reader{
+				rd:   bufio.NewReader(fd),
+				_buf: make([]byte, 64),
+			},
+		}
+	}
+	return client[address], nil
+}
+
 // PING ...
-func PING(conn *OConn, address string) string {
+func PING(conn *OConn, address string) (str string) {
 	if conn == nil {
 		return "failed"
 	}
-	e := Write(conn, address, []byte("ping\r\n"))
+	connGet(conn)
+	defer connRelease(conn)
+	e := conn.write("ping")
 	if e != nil {
-		return "failed"
+		log.FMTLog(log.LOGERROR, e.Error())
+		return ""
 	}
-	head := readCRLF(conn)
-	if len(string(head)) >= 4 {
-		return string(head)[0:4]
-	}
-	return string(head) // return pong
-}
-
-func readCRLF(conn *OConn) []byte {
-	conn.mutex.Lock()
-	defer conn.mutex.Unlock()
-	var index int
-	var all []byte
-	for i := 0; i < MAXSIZE; i++ {
-		var headByte = make([]byte, 1)
-		conn.conn.Read(headByte)
-		if headByte[0] == CRLF[0] { // CRLF
-			index++
-		} else if headByte[0] == CRLF[1] && index == 1 {
-			break
-		} else {
-			index = 0
-			if headByte[0] != '\x00' { // empty ascii
-				all = append(all, headByte[0])
-			}
-		}
-	}
-	return all
-}
-
-// Write ...
-func Write(conn *OConn, address string, bts []byte) (e error) {
-	conn.mutex.Lock()
-	defer conn.mutex.Unlock()
-	_, e = conn.conn.Write(bts)
+	e = conn.clrf()
 	if e != nil {
-		delete(client, address)
+		log.FMTLog(log.LOGERROR, e.Error())
+		return ""
 	}
+	line := conn.getline()
+	if len(line) == 0 {
+		return ""
+	}
+	str = string(line)
 	return
 }
 
@@ -97,21 +120,76 @@ func Count(conn *OConn, address string) (result []string) {
 	if conn == nil {
 		return []string{"failed"}
 	}
-	e := Write(conn, address, []byte("get\r\n"))
-	if e != nil {
-		return
-	}
-	head := readCRLF(conn)
-	if len(head) == 0 {
-		return
-	}
-	len, e := strconv.Atoi(string(head))
+	connGet(conn)
+	defer connRelease(conn)
+	e := conn.write("get")
 	if e != nil {
 		log.FMTLog(log.LOGERROR, e.Error())
+		return []string{""}
 	}
-	for i := 0; i < len*2; i += 2 {
-		result = append(result, string(readCRLF(conn)))
-		result = append(result, string(readCRLF(conn)))
+	e = conn.clrf()
+	if e != nil {
+		log.FMTLog(log.LOGERROR, e.Error())
+		return []string{""}
+	}
+	line := conn.getline()
+	if len(line) == 0 {
+		return []string{""}
+	}
+	count, _ := strconv.Atoi(string(line))
+	for i := 0; i < count; i++ {
+		key := conn.getline()
+		value := conn.getline()
+		result = append(result, string(key))
+		result = append(result, string(value))
 	}
 	return
+}
+
+func (conn *OConn) getline() []byte {
+	line, isPrefix, err := conn.rd.rd.ReadLine()
+	if err != nil {
+		log.FMTLog(log.LOGERROR, err.Error())
+		RefreshClient(conn.address)
+		return []byte{}
+	}
+	if isPrefix {
+		log.FMTLog(log.LOGERROR, bufio.ErrBufferFull.Error())
+		return []byte{}
+	}
+	if len(line) == 0 {
+		return []byte{}
+	}
+	return line
+}
+
+func (conn *OConn) write(arg interface{}) (e error) {
+	switch arg.(type) {
+	case []byte:
+		_, e = conn.wr.wr.Write(arg.([]byte))
+	case string:
+		_, e = conn.wr.wr.WriteString(arg.(string))
+	case byte:
+		e = conn.wr.wr.WriteByte(arg.(byte))
+	case rune:
+		_, e = conn.wr.wr.WriteRune(arg.(rune))
+	default:
+		break
+	}
+	if e != nil {
+		log.FMTLog(log.LOGERROR, e.Error())
+		RefreshClient(conn.address)
+	}
+	return
+}
+func (conn *OConn) clrf() (e error) {
+	e = conn.write(CRLF[0])
+	if e != nil {
+		return e
+	}
+	e = conn.write(CRLF[1])
+	if e != nil {
+		return e
+	}
+	return conn.wr.wr.Flush()
 }
